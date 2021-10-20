42f62b3abaa698e9d2e2fb30fc22bc0dc9851764
Jesus Saul Hernandez <jesus.castaneda@metricamovil.com>
2021-10-20 17:57:09 -0500

    Del Public

--- server.js
+++ server.js
@@ -87,9 +87,5 @@ async function startServer() {
   }
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve( __dirname, 'public/index.html'));
-});
-
 if (!process.env.test) startServer()
 module.exports = app
