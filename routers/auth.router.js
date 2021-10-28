2383189aca473af6c4a08a4959ed140b3b4c9c7e
CarlitosDav <90803444+CarlitosDav@users.noreply.github.com>
2021-10-28 16:26:52 -0500

    Creación del método renew para regenerar token

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const AuthMiddleware = require('../middlewares/auth.middleware');
 const AuthController = require('../controllers/auth.controller');
 
-router.post('/login', AuthMiddleware.login, AuthController.login)
+router.post('/login', AuthMiddleware.login, AuthController.login);
+router.get('/renew', AuthMiddleware.renew, AuthController.renew);
 
 module.exports = router;
