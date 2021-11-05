3c9fd63f79e2b203526eab42126676b9b1dec610
CarlitosDav <90803444+CarlitosDav@users.noreply.github.com>
2021-11-04 18:01:51 -0600

    Creación de repositorio Covid

--- repositories/covid.repository.js
+++ repositories/covid.repository.js
@@ -18,6 +18,76 @@ class CovidRepository {
       console.log(error);
     }
   }
+
+  getActiveCasesDay = async(country) => {
+    try {
+      let today = moment(new Date(2021, 9, 27)).format("DD-MM-YYYY")
+      let resp = CovidActiveCasesDay.findOne({ dateQuery: today, country: country });
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getCases = async(country) => {
+    try {
+      let today = moment(new Date(2021, 9, 27)).format("DD-MM-YYYY")
+      let resp = CovidCase.findOne({ dateQuery: today, country: country });
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getDeaths = async(country) => {
+    try {
+      let today = moment(new Date(2021, 9, 27)).format("DD-MM-YYYY")
+      let resp = CovidDeath.findOne({ dateQuery: today, country: country });
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getRecoveredSum = async(country) => {
+    try {
+      let today = moment(new Date(2021, 9, 27)).format("DD-MM-YYYY")
+      let resp = CovidRecoveredSum.findOne({ dateQuery: today, country: country });
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getRecoveredDay = async(country) => {
+    try {
+      let today = moment(new Date(2021, 9, 27)).format("DD-MM-YYYY")
+      let resp = CovidRecoveredDay.findOne({ dateQuery: today, country: country });
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getTestSum = async(country) => {
+    try {
+      let today = moment(new Date(2021, 9, 27)).format("DD-MM-YYYY")
+      let resp = CovidTestSum.findOne({ dateQuery: today, country: country });
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  getTestDay = async(country) => {
+    try {
+      let today = moment(new Date(2021, 9, 27)).format("DD-MM-YYYY")
+      let resp = CovidTestDay.findOne({ dateQuery: today, country: country });
+      return resp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = new CovidRepository()
